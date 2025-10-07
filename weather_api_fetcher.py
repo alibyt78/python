4e5bd7e7d378978bbacacb4ba2fc0c9478992365
alibyt78 <53123987+alibyt78@users.noreply.github.com>
2025-10-07 23:13:14 +0330

    Update weather_api_fetcher.py
    new version of api

--- weather_api_fetcher.py
+++ weather_api_fetcher.py
@@ -1,2 +1,15 @@
-# New Python script example
-print('Script placeholder')
+import requests
+
+API_KEY = 'your_api_key'
+BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
+
+def get_weather(city):
+    params = {'q': city, 'appid': API_KEY, 'units': 'metric'}
+    response = requests.get(BASE_URL, params=params)
+    data = response.json()
+    return data
+
+if __name__ == "__main__":
+    city = input("Enter city: ")
+    weather = get_weather(city)
+    print(weather)
